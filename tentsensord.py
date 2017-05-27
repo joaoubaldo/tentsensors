6790c674ee4bf9548ec43555ecfa677f0058d681
Joao Coutinho <me@joaoubaldo.com>
2017-05-27 19:16:19 +0100

    new: added useful functions to tentsensord new: state and last update time is saved in memory new: night and day logic separation new: tentsensord configuration loaded from json file new: sighup reloads config, sigterm stops everything

--- tentsensord.py
+++ tentsensord.py
@@ -1,40 +1,66 @@
 from datetime import datetime
+from datetime import timedelta
 import time
+import json
+import sys
+import os
+import signal
 
 import mysensors.mysensors as mysensors
+from mysensors import Message
 from mysensors.const_20 import MessageType
 from mysensors.const_20 import Presentation
 from mysensors.const_20 import SetReq
 from mysensors.const_20 import Internal
 
-
-child_map = {
-'hum1': 10,
-'hum2': 11,
-'temp1': 12,
-'temp2': 13,
-'Resistor': 14,
-'Fan': 15,
-'Extractor': 16,
-'LED': 17,
-'HPS': 18,
-'Humidifier': 19
-}
-
 current_state = {}
+config_file = ''
+config = {}
+gw_thread = None
 
-
-logic = {
-"day_start": 16,
-"day_end": 6,
-"target_night_temp": 20,
-"target_day_temp": 25,
-}
 
 def child_name_by_id(id_):
-    for k, v in child_map.items():
+    for k, v in config['child_map'].items():
         if v == int(id_):
             return k
+    return None
+
+
+def is_day(now=None):
+    if not now:
+        now = datetime.now()
+
+    start = config['day_start']
+    end = config['day_end']
+
+    if start > end:
+        return now >= now.replace(hour=start) or now < now.replace(hour=end)
+    else:
+        return now >= now.replace(hour=start) and now < now.replace(hour=end)
+
+
+def update_child_state(child_id, value, message=None):
+    global current_state
+
+    if message:
+        value = message.payload
+        child_id = message.child_id
+
+    print('Setting', child_id, 'to', value)
+
+    current_state[child_name_by_id(child_id)] = {
+        'value': str(value),
+        'last_update': datetime.now()
+    }
+
+def last_update(device):
+    if device in current_state:
+        return current_state[device]['last_update']
+    return None
+
+def device_value(device):
+    if device in current_state:
+        return current_state[device]['value']
     return None
 
 
@@ -49,31 +75,76 @@ def event(message):
         else:
             print("%s is a %s" % (child_name_by_id(message.child_id), message.sub_type))
     elif command == MessageType.set:
-        current_state[child_name_by_id(message.child_id)] = message.payload
-        print(current_state)
+        update_child_state(None, None, message)
 
 
-GATEWAY = mysensors.SerialGateway('/dev/ttyUSB0', event, protocol_version='1.5', baud=115200)
-GATEWAY.start()
-
-""" give a few seconds for gw to startup """
-time.sleep(5)
+def turn_on(gw, device):
+    gw.set_child_value(0, config['child_map'][device], 2, 1)
 
 
-def is_day(now=None):
-    if not now:
-        now = datetime.now()
-    start = 16
-    end = 6
-    if start > end:
-        return now >= now.replace(hour=start) or now < now.replace(hour=end)
-    else:
-        return now >= now.replace(hour=start) and now < now.replace(hour=end)
+def turn_off(gw, device):
+    gw.set_child_value(0, config['child_map'][device], 2, 0)
+
+def toggle(gw, device):
+    if device_value(device) and device_value(device) == '1':
+        turn_off(gw, device)
+    elif device_value(device) and device_value(device) == '0':
+        turn_on(gw, device)
 
 
-while True:
-    if is_day():
-        print("day logic...")
-    else:
-        print("night logic...")
-    time.sleep(2)
+def day_logic(gw):
+    now = datetime.now()
+    if not last_update('Extractor') or (now - last_update('Extractor')) >= timedelta(seconds=5):
+        toggle(gw, 'Extractor')
+
+
+def night_logic(gw):
+    pass
+
+
+def main():
+    global gw_thread
+
+    gw_thread = mysensors.SerialGateway(config["device"], event, protocol_version='1.5', baud=config["baud_rate"])
+    gw_thread.start()
+    
+    """ give a few seconds for gw to startup """
+    time.sleep(5)
+
+    while gw_thread.is_alive():
+        if is_day():
+            day_logic(gw_thread)
+        else:
+            night_logic(gw_thread)
+        time.sleep(config['loop_sleep'])
+
+
+def sighup_handler(signum, frame):
+    global config, config_file
+    try:
+        """ TODO: protect against changes to some attributes """
+        config = json.load(open(config_file, 'r'))
+        print('SIGHUP: Reloading config from', config_file)
+    except Exception:
+        gw_thread.stop()
+        raise
+
+def sigterm_handler(signum, frame):
+    print('SIGTERM: stopping...')
+    gw_thread.stop()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: %s <config file>" % (sys.argv[0],))
+        sys.exit(2)
+
+    config_file = sys.argv[1]
+    config = json.load(open(config_file, 'r'))
+
+    for device in config['child_map'].values():
+        update_child_state(device, 0)
+
+    signal.signal(signal.SIGHUP, sighup_handler)
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    
+    main()
